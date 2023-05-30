a3c12b76e2657be8e75bada600d5617857ad212b
anmedina-arg <andres.medina.arg@gmail.com>
2023-05-29 21:07:33 -0300

    finish logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,10 +18,10 @@ const WINNER_COMBOS = [
   [2, 4, 6]
 ];
 
-function Square({ children, isSelected, updateBoard, index }: any): any {
+function Square({ children, isSelected, updateBoard = null, index }: any): any {
 
   const handleClick = () => {
-    updateBoard(index)
+    if (updateBoard) return updateBoard(index)
   };
 
   return (
@@ -35,9 +35,19 @@ function Square({ children, isSelected, updateBoard, index }: any): any {
 
 
 export default function Home() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [turno, setTurno] = useState(TURNOS.X);
-  const [winner, setWinner] = useState(null);
+  const [board, setBoard] = useState(() => {
+    const boardFromLocalStorage = window.localStorage.getItem('board');
+    return boardFromLocalStorage
+      ? JSON.parse(boardFromLocalStorage)
+      : Array(9).fill(null)
+  })
+
+  const [turno, setTurno] = useState(() => {
+    const turnoFromLocalStorage = window.localStorage.getItem('turno');
+    return turnoFromLocalStorage ? JSON.parse(turnoFromLocalStorage) : TURNOS.X
+  });
+
+  const [winner, setWinner] = useState<null | boolean>(null);
 
   const checkWinner = (checkBoard: any) => {
 
@@ -52,7 +62,11 @@ export default function Home() {
       }
     }
     return null
-  }
+  };
+
+  const checkGame = (boardToCheck: any) => {
+    return boardToCheck.every((elements: any) => elements !== null)
+  };
 
   const updateBoard = (index: any) => {
 
@@ -65,13 +79,30 @@ export default function Home() {
     let nuevoTurno = turno === TURNOS.X ? TURNOS.O : TURNOS.X;
     setTurno(nuevoTurno);
 
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turno', JSON.stringify(nuevoTurno))
+
     const newWinner = checkWinner(newBoard);
-    setWinner(newWinner)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkGame(newBoard)) {
+      setWinner(false)
+    }
+  };
+
+  const restartGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurno(TURNOS.X);
+    setWinner(null)
   };
 
   return (
     <main className="flex flex-col justify-center items-center h-screen">
       <h1 className="mb-4">Ta - Te - Ti</h1>
+
+      <button className="border-2 border-solid border-gray-400 rounded-md" onClick={restartGame}>
+        Resetear el juego
+      </button>
 
       <section className="grid grid-cols-3">
         {
@@ -93,6 +124,29 @@ export default function Home() {
         <Square isSelected={turno === TURNOS.X} >{TURNOS.X}</Square>
         <Square isSelected={turno === TURNOS.O} >{TURNOS.O}</Square>
       </section>
+
+      {
+        winner !== null &&
+        (
+          <section className="absolute bg-white border-2 border-black rounded-md p-4 flex flex-col justify-center items-center">
+            <div>
+              <h2 className="text-center">
+                {
+                  winner === false
+                    ? 'Empate'
+                    : 'Ganó: '
+                }
+              </h2>
+              <header className="w-fit m-auto">
+                {winner && <Square>{winner}</Square>}
+              </header>
+              <button onClick={restartGame} className="border-2 border-gray-500 rounded-md p-1">
+                Empezar de nuevo
+              </button>
+            </div>
+          </section>
+        )
+      }
 
     </main>
   )
